868ecee4def90e54bddd81de89a2f616eeb17cda
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-02 15:45:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,27 +7,23 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each { |item| print_items(item) }
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if !@@cart.empty?
-        @@cart.each { |item| print_items(item) }
-      else
-        resp.write "Your cart is empty."
-      end
+      if !cart.empty?
+        @@cart.each { |item| resp.write "#{item}\n"
+    
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
   end
-  
-  def print_items(item)
-    resp.write "#{item}\n"
-  end
-    
+
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
