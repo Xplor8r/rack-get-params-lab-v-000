dc118b9082580bd9cdb5b5ecf1f4c56db33e7ec4
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-02 15:30:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if !cart.empty?
-        @@cart.each { |item| resp.write
+        @@cart.each { |item| resp.write "#{item}\n"
     
     else
       resp.write "Path Not Found"
